43688442597f3ed06c08ade9abda0bca4abbcefd
ahmyk9 <ahmedkhilji99@gmail.com>
2024-01-18 16:41:09 +1100

    update fixed 5

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    domains: ['image.tmdb.org'],
+
+  images: {
+    domains: ['rb.gy', 'image.tmdb.org'], // Add 'rb.gy' to the list of domains
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+      },
+    ],
   },
 }
 
